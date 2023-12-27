208f24125dd679502d708c1a133efcdf8787b00e
Elvis Brevi <elvis.brevi@sag.gob.cl>
2023-12-27 17:51:00 -0300

    refactor: improve microfrontend architecture and rename services

--- microfrontends/card-app/src/App.jsx
+++ microfrontends/card-app/src/App.jsx
@@ -1,45 +1,13 @@
-import React, { useState } from 'react';
-import Card from 'cardComponent/Card';
-import axios from 'axios';
+import { useState, useRef } from 'react';
+import Card from 'cardComponent/Card'; 
 
 function App() {
   const [cardName, setCardName] = useState('');
-  const [cardData, setCardData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [dataOrigin, setDataOrigin] = useState('');
+  const inputRef = useRef(null);
 
-  const fetchCardData = async () => {
-    setLoading(true);
-    try {
-      // Intenta obtener la tarjeta desde la caché de Redis
-      let response = await axios.get(`http://localhost:3002/cache/card:${cardName}`);
-      let data = response.data;
-
-      // Si la caché está vacía, obtén la tarjeta desde la API de Hearthstone
-      if (!data || Object.keys(data).length === 0) {
-        response = await axios.get(`http://localhost:3001/card/${cardName}`);
-        data = response.data[0];
-
-        // Guarda la tarjeta en Redis para futuras consultas
-        await axios.post('http://localhost:3002/cache', {
-          cacheKey: `card:${cardName}`,
-          cardInfo: {
-            name: data.name,
-            artist: data.artist,
-            img: data.img
-          }
-        });
-
-        setDataOrigin('API de Hearthstone');
-      } else {
-        setDataOrigin('Caché de Redis');
-      }
-
-      setCardData(data);
-    } catch (error) {
-      console.error("Error al obtener datos de la tarjeta", error);
-    } finally {
-      setLoading(false);
+  const handleButtonClick = () => {
+    if (inputRef.current) {
+      setCardName(inputRef.current.value);
     }
   };
 
@@ -47,16 +15,13 @@ function App() {
     <div>
       <input
         type="text"
-        value={cardName}
-        onChange={(e) => setCardName(e.target.value)}
+        ref={inputRef}
         placeholder="Ingrese nombre de la carta"
       />
-      <button onClick={fetchCardData} disabled={loading}>
-        {loading ? 'Cargando...' : 'Buscar Carta'}
-      </button>
 
-      <p>Datos obtenidos desde: {dataOrigin}</p>
-      <Card cardData={cardData} />
+      <button onClick={handleButtonClick}>Buscar</button>
+
+      <Card name={cardName} />
     </div>
   );
 }
--- microfrontends/card-component/src/components/Card.jsx
+++ microfrontends/card-component/src/components/Card.jsx
@@ -1,12 +1,54 @@
-import "./Card.css"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Card({ cardData }) {
+// eslint-disable-next-line react/prop-types
+function Card({ name }) {
+  const [cardData, setCardData] = useState(null);
+  const [origin, setOrigin] = useState(null);
+
+  useEffect(() => {
+    const fetchCardData = async () => {
+      if (!name) return;
+
+      try {
+        let response = await axios.get(`http://localhost:3002/cache/card:${name}`);
+        let data = response.data;
+
+        if (!data || Object.keys(data).length === 0) {
+          response = await axios.get(`http://localhost:3001/card/${name}`);
+          data = response.data[0];
+
+          await axios.post('http://localhost:3002/cache', {
+            cacheKey: `card:${name}`,
+            cardInfo: {
+              name: data.name,
+              artist: data.artist,
+              img: data.img
+            }
+          });
+
+          setOrigin('API de Hearthstone');
+        } else {
+          setOrigin('Caché de Redis');
+        }
+
+        setCardData(data);
+        
+      } catch (error) {
+        console.error("Error al obtener datos de la carta", error);
+      }
+    };
+
+    fetchCardData();
+  }, [name]); 
+
   if (!cardData) {
-    return null; // o algún placeholder si prefieres
+    return null; 
   }
 
   return (
     <div>
+      <p>Origen de los datos: {origin}</p>
       <h3>{cardData.name}</h3>
       {cardData.artist && <p>Artista: {cardData.artist}</p>}
       {cardData.img && <img src={cardData.img} alt={cardData.name} />}
@@ -14,4 +56,4 @@ function Card({ cardData }) {
   );
 }
 
-export default Card
+export default Card;
